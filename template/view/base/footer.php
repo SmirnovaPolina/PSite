322df8f0879eb16ac22d01230005e2378d68744d
SmirnovaPolina <31035109+SmirnovaPolina@users.noreply.github.com>
2017-09-27 18:10:02 +0300

    Add files via upload

---
+++ template/view/base/footer.php
@@ -0,0 +1,11 @@
+			</div>
+		</section>
+		<footer>
+			<div class='content'>
+				<div class='copyright'>
+					2017 © ООО "Рога и Копыта"
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ template/view/base/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>TITLE</title>
+		<meta name="description" content="DESCRIPTION"/>
+		<meta name="keywords" content="KEYWORDS"/>
+		<link rel='stylesheet' type='text/css' href='http://<?=$_SERVER['HTTP_HOST'].'/website'?>/template/view/css/style.css' media='all' />
+	</head>
+	<body>
+		<header>
+			<div class='content'>
+				<div class='logo'>
+					<img src='logo.png' alt='logo'>
+				</div>
+				<div class='wrap-header'>
+					<div class='phone'>
+						<p>Телефон<br>+7 988 888 88 16</p>
+					</div>
+				<div class='button callback'><a href='#'>Заказать звонок</a></div>
+				<nav>
+					<ul class='main-menu'>
+						<a href='#'><li>Пункт меню</li></a>
+						<a href='#'><li>Пункт меню</li></a>
+						<a href='#'><li>Пункт меню</li></a>
+						<a href='#'><li>Пункт меню</li></a>
+					</ul>
+				</nav>
+				</div>
+			</div>
+		</header>
+		<section>
+			<div class='content'>
